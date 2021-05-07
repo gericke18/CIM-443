b0d0e4a1401e3ae17c73746c629913b00edf3321
TalonCSmith <taloncsmith01@gmail.com>
2021-05-07 13:09:13 -0400

    Click planes to open link

---
+++ Miami Tourism Map/Assets/Scripts/Fountainbleu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Fountainbleu : MonoBehaviour
+{
+    [SerializeField] UnityEvent fClicked;
+
+    //Invoke action
+    private void OnMouseDown(){
+        Application.OpenURL("https://www.fontainebleau.com/");
+        // fClicked.Invoke();
+    }
+    
+    //open link
+    // public void EventClick(){
+    //     Application.OpenURL("https://www.fontainebleau.com/");
+    // }
+}
